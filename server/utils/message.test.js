f2a37716cecfa2665c387fd8e407e738021fd60d
Braedon Gough <braedongough@Braedons-MacBook-Pro.local>
2018-09-23 12:30:10 +0200

    Add generateLocationMessage with test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect')
 
-const {generateMessage} = require('./message')
+const {generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
     it('should generate the correct message object', () => {
@@ -14,5 +14,21 @@ describe('generateMessage', () => {
             text
         })
         expect(typeof message.createdAt).toBe('number')
+    })
+})
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        const from = 'Braedon'
+        const lat = 1
+        const long = 4
+        const url = 'https://www.google.com/maps/search/1,4'
+        const locationMessage = generateLocationMessage(from, lat, long)
+
+        expect(locationMessage).toMatchObject({
+            from,
+            url
+        })
+        expect(typeof locationMessage.createdAt).toBe('number')
     })
 })
